8657a644553a8b0f87454a753d4f3b90c7d264da
jh27kim <jh27kim@gmail.com>
2022-03-14 17:44:55 +0900

    RollBack

--- BE/articleTopic/articleHeatMap/views.py
+++ BE/articleTopic/articleHeatMap/views.py
@@ -2,18 +2,3 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 
-from .models import Article
-
-
-def index(request):
-    return render(request, 'todo/index.html', {})
-
-
-def articleList(request):
-    aritcle = Article.objects.all()
-    article_list = []
-
-    for index, arti in enumerate(aritcle, start=1):
-        article_list.append({'id': index, 'title': arti.title})
-
-    return JsonResponse(article_list, safe=False)
